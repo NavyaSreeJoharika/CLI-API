45d5d7a54dfbd8dbbec3adeda7f51d057cb7ce6d
Navya Sree Joharika Gompa <navyasree.gompa@synectiks.com>
2024-08-01 17:04:37 +0530

    commit

--- ShortenURL/url2/main.go
+++ ShortenURL/url2/main.go
@@ -8,22 +8,24 @@ import (
 	"time"
 )
 
-var urlMap = make(map[string]string)
+var urlMap = make(map[string]string) //an empty map is created
 
 const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 func init() {
-	rand.Seed(time.Now().UnixNano())
+	rand.Seed(time.Now().UnixNano()) //Without this line, the output would be the same every time the program runs.
 }
 
 // generateShortLink generates a random string for the short link.
-func generateShortLink(n int) string {
-	b := make([]byte, n)
+func generateShortLink(n int) string { //func function_name(Parameter-list)(Return_type){
+	b := make([]byte, n) //SLICE  an empty slice is created with byte data type ,n is length of the slice
 	for i := range b {
 		b[i] = letterBytes[rand.Intn(len(letterBytes))]
 	}
+	//everythime a new index number is generated and stored in b slice .
 	return string(b)
-}
+	//When you use string(b), it converts each byte in the slice to its corresponding character in the resulting string.
+} //uptill this inside everything is called function body, here is close of a function
 
 // shortenURLHandler handles the URL shortening.
 func shortenURLHandler(w http.ResponseWriter, r *http.Request) {
@@ -35,11 +37,11 @@ func shortenURLHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	shortURL := generateShortLink(6)
+	shortURL := generateShortLink(6) //shorturl="anNIge"
 	urlMap[shortURL] = request.URL
 
-	fmt.Printf("URL Map afetr adding: %+v\n", urlMap)
-
+	fmt.Printf("URL Map afetr adding: %+v\n", urlMap) //URL Map afetr adding: map[anNIge:http://example99.com]
+	// %+v: This prints the value along with the field names (for structs)
 	response := map[string]string{"short_url": "http://localhost:8080/" + shortURL}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(response)
@@ -51,8 +53,8 @@ func originalURLHandler(w http.ResponseWriter, r *http.Request) {
 
 	originalURL, exists := urlMap[shortURL]
 
-	fmt.Printf("URL Map during retrieval: %+v\n ", urlMap)
-	fmt.Printf("Short URL: %s, Ecists: %v\n", shortURL, exists)
+	fmt.Printf("URL Map during retrieval: %+v\n ", urlMap)      //URL Map during retrieval: map[anNIge:http://example99.com]
+	fmt.Printf("Short URL: %s, Ecists: %v\n", shortURL, exists) //Short URL: anNIge, Ecists: true
 
 	if !exists {
 		http.NotFound(w, r)
